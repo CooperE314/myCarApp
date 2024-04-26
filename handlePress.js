9304c8cfff03c96d0f00f3120319ad61f632ec1b
eyarcade <143421183+eyarcade@users.noreply.github.com>
2024-04-26 11:51:13 -0400

    adding openCamera function

--- handlePress.js
+++ handlePress.js
@@ -1,24 +1,42 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { View, Button, StyleSheet } from 'react-native';
 import { RNCamera } from 'react-native-camera';
 import { ViewPropTypes } from 'deprecated-react-native-prop-types';
 
+const handlePress = () => {
+  const videoRef = useRef(null);
 
-function handlePress() {
-  console.log('Button pressed!');
+  useEffect(() => {
+    const openCamera = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        if (videoRef.current) {
+          videoRef.current.srcObject = stream;
+        }
+      } catch (error) {
+        console.error('Error accessing camera:', error);
+      }
+    };
 
-  /*return (
-    <RNCamera
-      style={{ flex: 1 }}
-      type={RNCamera.Constants.Type.back}
-      captureAudio={false}
-      autoFocus={RNCamera.Constants.AutoFocus.on}
-      onGoogleVisionBarcodesDetected={({ barcodes }) => {
-        console.log(barcodes);
-      }}
-    />
+    openCamera();
+
+    // Cleanup function to stop the camera stream when component unmounts
+    return () => {
+      if (videoRef.current && videoRef.current.srcObject) {
+        const tracks = videoRef.current.srcObject.getTracks();
+        tracks.forEach(track => track.stop());
+      }
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>Camera App</h1>
+      <video ref={videoRef} autoPlay playsInline></video>
+    </div>
   );
-  */
-}
+};
 
 export default handlePress;
+
+
