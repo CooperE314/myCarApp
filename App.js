37c86ea8c73382912eacd057580fb203a5e0007c
Cooper <CooperE314@github.com>
2024-05-23 10:54:42 -0700

    can flip screen and take foto

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
 import { Camera, CameraType } from 'expo-camera';
-import { useState } from 'react';
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { Button, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   const [type, setType] = useState(Camera.Constants.Type.back);
   const [permission, requestPermission] = Camera.useCameraPermissions();
+  const cameraRef = useRef(null);
+  const [photoUri, setPhotoUri] = useState(null);
 
-  if (!permission) {
+
     // Camera permissions are still loading
+  if (!permission) {
     return <View />;
   }
-
-  if (!permission.granted) {
     // Camera permissions are not granted yet
+  if (!permission.granted) {
     return (
       <View style={styles.container}>
         <Text style={{ textAlign: 'center' }}>
@@ -22,29 +24,49 @@ export default function App() {
       </View>
     );
   }
-
-
+  //flip camera function
   function toggleCameraType() {
     setType((current) => (
       current === CameraType.back ? CameraType.front : CameraType.back
     ));
   }
+  const takePicture = async () => {
+    if (cameraRef.current) {
+      const photo = await cameraRef.current.takePictureAsync();
+      setPhotoUri(photo.uri);
+    }
+  };
+  
 
   return (
-    <View style={styles.container}>
-      <Camera style={styles.camera} type={type}>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={toggleCameraType}>
-            <Text style={styles.text}>Flip Camera</Text>
-          </TouchableOpacity>
-        </View>
-      </Camera>
+   <View style={styles.container}>
+      {photoUri ? (
+        <Image source={{ uri: photoUri }} style={styles.photo} />
+      ) : (
+        <Camera
+          style={styles.camera}
+          type={type}
+          ref={cameraRef}
+        >
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={toggleCameraType}
+            >
+              <Text style={styles.text}>Flip Camera</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={takePicture}
+            >
+              <Text style={styles.text}>Take Picture</Text>
+            </TouchableOpacity>
+          </View>
+        </Camera>
+      )}
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -68,5 +90,9 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
     color: 'white',
+  },
+  photo: {
+    flex: 1,
+    resizeMode: 'contain',
   },
 });
